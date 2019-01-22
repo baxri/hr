a2b2c670881642ea258d379651f43038e0283201
George Bibilashvili <giorgi.bibilashvili89@gmail.com>
2019-01-22 16:47:46 +0400

    Add default role for imported users

--- app/User.php
+++ app/User.php
@@ -6,6 +6,7 @@ use Backpack\CRUD\CrudTrait;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Support\Facades\DB;
 use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
@@ -29,4 +30,18 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($user) {
+            DB::table('model_has_roles')->insert([
+                'role_id' => 2,
+                'model_type' => 'App\Models\BackpackUser',
+                'model_id' => $user->id,
+            ]);
+        });
+    }
+
 }
